d77f57e032c2f825a94f98364930d476470cc331
JJ-Bas <jef.j.bas@gmail.com>
2023-01-19 16:58:16 +0100

    loops.js opdrachten afgerond

--- loops.js
+++ loops.js
@@ -6,7 +6,9 @@
 // Hoera!
 // ==========================================
 
-
+for (let i = 0; i < 3; i++) {
+    console.log("Hoera!")
+}
 
 // ==========================================
 // Opdracht 2. Maak een for-loop die 4 keer het woord "loop..." logt, en bij de vijfde loop het woord "klaar!"
@@ -16,9 +18,15 @@
 // loop...
 // loop...
 // klaar!
- // ==========================================
+// ==========================================
 
-
+for (let i = 0; i < 5; i++) {
+    if (i < 4) {
+        console.log("loop...")
+    } else {
+        console.log("klaar")
+    }
+}
 
 // ==========================================
 // Opdracht 3. Maak een for-loop die van 0 tot 15 loopt. Voor ieder getal wordt in de console gelogd of het getal even of oneven is.
@@ -30,9 +38,15 @@
 // .... etc.
 // 15 is oneven
 
+for (let i = 0; i < 15; i++) {
+    if (i % 2 === 0) {
+        console.log("even")
+    } else {
+        console.log("oneven")
+    }
+}
 // Tip: een getal is oneven als je het door 2 deelt en er blijft nog iets over. Een getal is even als je het deelt door 2 en er blijft niets over. Google daarom eens naar 'mdn webdocs remainder operator'.
 // ==========================================
-
 
 
 // ==========================================
@@ -50,4 +64,14 @@
 // >> 8
 // >> 9
 // ==========================================
-
+for (let i = 0; i < 10; i++) {
+    if (i < 3) {
+        console.log(i)
+    }
+    if (i >= 3 && i < 6) {
+        console.log(">" + i)
+    }
+    if ( i > 6) {
+        console.log(">>" + i)
+    }
+}
